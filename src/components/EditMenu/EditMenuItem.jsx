8660f57a30eaa786d5a5a2212d97e301e7456d51
Fedor Tarasenko <fedortarasenko2001@gmail.com>
2024-04-04 15:33:28 +0200

    EditMenuItem card done

--- src/components/EditMenu/EditMenuItem.jsx
+++ src/components/EditMenu/EditMenuItem.jsx
@@ -4,14 +4,14 @@ function EditMenuItem() {
 	return (
 		<>
 			<div className="edit-menu-item-container">
-				<img src={''} alt="img" className='course-img' />
+				<img src='./Masa1.png' alt="img" className='course-img' />
 				<div className="column">
 					<h3>Title</h3>
 					<p className="edit-menu-item-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore explicabo quod sint consectetur cum molestias impedit consequuntur nobis necessitatibus, unde suscipit beatae! Velit odit perspiciatis, minus consequuntur eveniet exercitationem tenetur?</p>
 				</div>
 				<div className="column2">
 					<button>
-						<img src={''} alt="" className='close' />
+						<img src='./Vector.png' alt="" className='close' />
 					</button>
 					<p className="edit-menu-item-price">20$</p>
 				</div>
