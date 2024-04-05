0fdf3dfddd09a706c74b22d5c638c74fd11a686d
Martin Fahlström <makrill88@gmail.com>
2024-04-05 10:58:26 +0200

    change to update logic

--- src/components/MenuPage/NavMenu.jsx
+++ src/components/MenuPage/NavMenu.jsx
@@ -8,11 +8,11 @@ function NavMenu() {
 
 	function handleGuestChange(event) {
 		const selectedGuests = event.target.value
-		const updatedCourses = selectedGuests;
+		// const updatedCourses = ;
         const updatedRequiredCourses = selectedGuests * 3;
         inc(); 
         set({ guest: selectedGuests,
-			 courses: updatedCourses,
+			//  courses: updatedCourses,
 			 requiredCourses: updatedRequiredCourses,
 			});
 	
@@ -29,8 +29,6 @@ function NavMenu() {
 			<option value="2">2</option>
 			<option value="3">3</option>
 			<option value="4">4</option>
-			<option value="5">5</option>
-			<option value="6">6</option>
 			</select>
 			</div>
 
--- src/components/MenuPage/navStore.js
+++ src/components/MenuPage/navStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand'
 
 export const navStore = create((set) => ({
 	guest: 1,
-	courses: 1,
+	courses: 0,
 	requiredCourses: 3,
 	inc: () => set((state) => ({ guests: state.select + 1 })),
 	set,
