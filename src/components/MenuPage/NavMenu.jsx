0ace01e45f3d75f7e03c91bcacf88fe8cace3d1f
Martin Fahlström <makrill88@gmail.com>
2024-04-05 09:56:05 +0200

    styling

--- src/components/MenuPage/NavMenu.jsx
+++ src/components/MenuPage/NavMenu.jsx
@@ -6,12 +6,12 @@ function NavMenu() {
 
 	return (
 
-		<header className='navbar'>
+		<header className='guest-navbar'>
 
 			<div>
 			<label> Guests: </label>
 			<select>
-			<option value="1">1  </option>
+			<option value="1">1</option>
 			<option value="2">2</option>
 			<option value="3">3</option>
 			<option value="4">4</option>
