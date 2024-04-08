ad5c13878effc9ad2d8b19c70881ec492f180b40
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-04-08 14:53:48 +0200

    Forsättning av merge på routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,33 @@ import MenuPage from './MenuPage/Menu'
 import EditMenu from './EditMenu/EditMenu'
 import LoginPage from './Login/Login'
 import Form from './Form/Form'
-import Footer from './Footer/Footer'
+
+import { Link, Route, Routes } from 'react-router-dom'
 
 function App() {
 
-  return (
-  <>
-      <LandingPage/>
-      <MenuPage />
-      <EditMenu />
-      <Footer />
-    </>
-  )
+	return (
+		<>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Hem</Link>
+						<Link to="/meny">Meny</Link>
+						<Link to="/redigera">Redigera</Link>
+						<Link to="/login">Login</Link>
+						<Link to="/form">Form</Link>
+					</li>
+				</ul>
+			</nav>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/meny" element={<MenuPage />} />
+				<Route path="/redigera" element={<EditMenu />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/form" element={<Form />} />
+			</Routes>
+		</>
+	)
 }
 
 export default App
