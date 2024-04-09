03a31e699a14a39a2558ee39e55441e3fede9c78
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-04-09 10:29:00 +0200

    Sticky nav menu är nu ordnad

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -10,21 +10,22 @@ export default function ShoppingCart({ shoppingCart, removeFromBasket }) {
     <div className='shopping-cart-container'>
       <h1>Shopping Cart</h1>
 	  <span className='back-arrow'>&#8656;</span>
-	  
+
       <ul className='shopping-cart-list'>
         {shoppingCart.map((item, index) => (
           <li className='shopping-cart-item' key={index}>
 			<img className='shopping-cart-img' src={item.image} alt="" />
 			<div className='shopping-cart-text'>
-				<button 
-				className='shopping-cart-remove-btn'
-				onClick={() => removeFromBasket(item)}>
-				<img src='../public/Vector.png' alt="x" />
-				</button>
 				<h2 className='shopping-cart-title'>{item.title}</h2>
 				<p className='shopping-cart-description'>{item.description}</p>
 				<p className='shopping-cart-price'>${item.price}</p>
 			</div>
+
+			<button
+				className='shopping-cart-remove-btn'
+				onClick={() => removeFromBasket(item)}>
+				<img src='../public/Vector.png' alt="x" />
+				</button>
           </li>
         ))}
       </ul>
