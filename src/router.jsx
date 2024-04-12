be6a1a203897e993224351a88dd21f84075a807a
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-04-12 10:24:56 +0200

    små ändringar i root

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,9 @@
 import { createHashRouter } from 'react-router-dom'
 import Root from './components/Root.jsx'
-import MenuPage from './components/MenuPage/Menu.jsx'
 import EditMenu from './components/EditMenu/EditMenu.jsx'
 import LoginPage from './components/Login/Login.jsx'
 import Form from './components/Form/Form.jsx'
 import ShoppingCart from './components/ShoppingCart/ShoppingCart.jsx'
-import Footer from './components/Footer/Footer.jsx'
 import LandingPage from './components/LandingPage/LandingPage.jsx'
 
 const router = createHashRouter([
