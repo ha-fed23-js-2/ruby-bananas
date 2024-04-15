4e0f4c8ce23241db9f18195d9903b3ff5e1c6ca1
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-04-15 10:14:52 +0200

    fixade login sidan så att den navigera till edit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './login.css';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
 
 const Letter = ({ letter, onLetterClick, isWrong }) => {
 	const [isClicked, setIsClicked] = useState(false);
@@ -23,20 +23,15 @@ const Letter = ({ letter, onLetterClick, isWrong }) => {
 
 const LoginPage = () => {
 	const [input, setInput] = useState('');
-	const [correctPassword, setCorrectPassword] = useState(false);
 	const [isWrong, setIsWrong] = useState(false);
 	const words = ["MASA", "FEJUJA"];
 	const password = "MUMS";
+	const navigate = useNavigate();
 
 	const handleLetterClick = (letter) => {
 		const newInput = input + letter;
 		if (newInput === password) {
-			setCorrectPassword(true);
-			setTimeout(() => {
-				setCorrectPassword(false);
-				setInput('');
-			}, 600);
-			alert('Välkommen!');
+			navigate('/edit'); 
 		} else if (!password.startsWith(newInput)) {
 			setIsWrong(true);
 			setInput('');
@@ -47,37 +42,28 @@ const LoginPage = () => {
 	};
 
 	return (
-
-
-		<section className='login-container'>
-
-
-				<header>
-					<div>
-						<Link to="/"><button>Back</button></Link>
-					</div>
-					<div className='logo'>
-						<img src="./logo.png" />
-					</div>
-
-				</header>
-
-		<div className={`logo ${correctPassword ? 'correct' : ''}`}>
-			{words.map((word, wordIndex) => (
-				<div key={wordIndex}>
-					{word.split('').map((letter, index) => (
-						<Letter key={index} letter={letter} onLetterClick={handleLetterClick} isWrong={isWrong} />
+		<>
+			<header>
+				<div>
+					<Link to="/"><button>Back</button></Link>
+				</div>
+				<div className='logo'>
+					<img src="./logo.png" alt="Logo" />
+				</div>
+			</header>
+			<section className='login-container'>
+				<div className={`passlogo ${isWrong ? 'wrong' : ''}`}>
+					{words.map((word, wordIndex) => (
+						<div key={wordIndex}>
+							{word.split('').map((letter, index) => (
+								<Letter key={index} letter={letter} onLetterClick={handleLetterClick} isWrong={isWrong} />
+							))}
+						</div>
 					))}
 				</div>
-
-
-			))}
-		</div>
-		</section>
-
-
+			</section>
+		</>
 	);
-
 };
 
 export default LoginPage;
