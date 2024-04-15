e4901ae4619fe269e948933c2ca60881fa7c38b0
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-04-15 15:13:36 +0200

    arbetat med footern

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,8 +9,14 @@ function Footer() {
 		<>
 			<section className='footerSection' id="footer">
 
-			<div className='footer-logo'>
-</div>
+			<div className='star-container'>
+
+					<img src='/MichelinStar.png'></img>
+					<img src='/MichelinStar.png'></img>
+					<img src='/MichelinStar.png'></img>
+					<img src='/MichelinStar.png'></img>
+					<img src='/MichelinStar.png'></img>
+			</div>
 
 
 
@@ -25,25 +31,33 @@ function Footer() {
 					<div className='footer-column'>
 						<h5 className='footer-title'>LOCATION</h5>
 						<span className='line'></span>
-						<h5> First Avenue</h5>
+						<h5>Strandvägen 1</h5>
+						<h5>Östermalm 112 30</h5>
+						<h5>Stockholm, Sweden</h5>
 					</div>
 
 					<div className='footer-column'>
-						<h5 className='footer-title'>CALL US</h5>
+						<h5 className='footer-title'>HOURS</h5>
 						<span className='line'></span>
-						<h5> First Avenue</h5>
+						<h5>Sundays: Closed</h5>
+						<h5>Mon-Thu 5:00 PM - 11:00 PM</h5>
+						<h5>Fri - Sat 5:00 PM - 10:00 PM</h5>
 					</div>
 
 					<div className='footer-column'>
-						<h5 className='footer-title'>LOCATION</h5>
+						<h5 className='footer-title'>PRESS & RESERVATION</h5>
 						<span className='line'></span>
-						<h5> First Avenue</h5>
+						<h5>+46 812 3456</h5>
+						<h5>reserve@masafejuja.com</h5>
+						<h5>press@theregaltable.com</h5>
 					</div>
 
 					<div className='footer-column'>
-						<h5 className='footer-title'>LOCATION</h5>
+						<h5 className='footer-title'>MASA FEJUJA GROUP AB</h5>
 						<span className='line'></span>
-						<h5> First Avenue</h5>
+						<h5>Nominated to Micheline Dominator</h5>
+						<h5>reserve@masafejuja.com</h5>
+						<h5>press@theregaltable.com</h5>
 					</div>
 
 
