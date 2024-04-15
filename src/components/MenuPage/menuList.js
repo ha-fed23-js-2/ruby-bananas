8510e5487bd37f153fd6f47e57ba0188e3c587fb
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2024-04-15 20:05:24 +0200

    ändrade text innehåll på rätterna

--- src/components/MenuPage/menuList.js
+++ src/components/MenuPage/menuList.js
@@ -3,31 +3,31 @@ const courses = [
 		id: 1,
 		name: "Forêt Noire Fungi Étagère",
 		description: "A tiered display of black foraged mushrooms resting on a petrified rye toast, complemented by a Chantilly of porcini essence.",
-		contains: "Contains: Foraged mushrooms, rye toast, porcini cream",
+		contains: "Contains: Foraged mushrooms, rye toast, porcini cream.",
 		image: "./Masa1.png",
 		price: 150
 	},
 	{
 		id: 2,
-		name: "Truffe et Céréale Ancienne",
-		description: "- An assortment of ancient grains crowned with a generous shaving of black truffle, encircled by a truffle-infused oil emulsion.",
+		name: "Caviar Tempura en Sphère",
+		description: "A crisp tempura orb filled with the finest Osetra caviar, perched atop a dollop of crème fraîche and garnished with micro greens.",
 		contains: "Contains: Ancient grains, black truffle, truffle oil.",
 		image: "./Masa2.png",
 		price: 175
 	},
 	{
 		id: 3,
-		name: "Caviar Tempura en Sphère",
-		description: "- A crisp tempura orb filled with the finest Osetra caviar, perched atop a dollop of crème fraîche and garnished with micro greens.",
+		name: "Coquilles d'agrumes",
+		description: "Citrus-marinated scallops en croute, served with a foam of aged Parmigiano-Reggiano, punctuated with a lemon zest confit.",
 		image: "./Masa3.png",
-		contains: "Contains: Kumquat, sturgeon caviar, gold leaf, dill",
-		price: 20.00
+		contains: "Contains: Scallop, yuzu, white balsamic, micro-herbs.",
+		price: 210
 	},
 	{
 		id: 4,
-		name: "Kumquat et Caviar",
-		description: "- A kumquat reduction base supporting a delicate caviar-infused sphere, adorned with gold leaf and a single dill frond.",
-		contains: "Contains: Kumquat, sturgeon caviar, gold leaf, dill",
+		name: "Langouste à la Royale",
+		description: "A majestic langoustine, poached in a bouquet garni-infused broth, presented in its natural form. Accompanied by a duo of sauces: saffron beurre blanc and a delicate crustacean reduction.",
+		contains: "Contains: Langoustine, bouquet garni, saffron, butter.",
 		image: "./Masa4.png",
 		price: 220
 	},
@@ -35,7 +35,7 @@ const courses = [
 		{
 		id: 5,
 		name: "Saint-Jacques en Fleurette",
-		description: "- Thinly sliced scallops arranged in a floral pattern, anointed with a yuzu and white balsamic vinaigrette, speckled with micro-herbs",
+		description: "Thinly sliced scallops arranged in a floral pattern, anointed with a yuzu and white balsamic vinaigrette, speckled with micro-herbs",
 		contains: "Contains: Scallop, yuzu, white balsamic, micro-herbs.",
 		image: "./Masa5.png",
 		price: 180
@@ -44,8 +44,8 @@ const courses = [
 	{
 		id: 6,
 		name: "Coquilles d'agrumes",
-		description: "- Citrus-marinated scallops en croute, served with a foam of aged Parmigiano-Reggiano, punctuated with a lemon zest confit.",
-		contains: "Contains: Scallops, citrus, Parmigiano-Reggiano, lemon confit",
+		description: "A royal selection of heirloom peas crowned with a buttery pearl onion, served in a hand-painted porcelain cup.",
+		contains: "Contains: Heirloom peas, pearl onions, butter, porcelain vessel.",
 		image: "./Masa6.png",
 		price: 165
 	}
